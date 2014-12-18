e4df8e912f1ff601c9632f89e6997019c8d6e891
sammyboy45467 <jeremy.robles56@gmail.com>
2014-12-18 09:35:27 -0800

    major update

--- www/js/app.js
+++ www/js/app.js
@@ -1,21 +1,4 @@
-/*
- * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
- * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
- * to you under the Apache License, Version 2.0 (the
- * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
- *
- * http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing,
- * software distributed under the License is distributed on an
- * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
- * KIND, either express or implied.  See the License for the
- * specific language governing permissions and limitations
- * under the License.
- */
+
 var app = {
     // Application Constructor
     initialize: function() {
@@ -80,13 +63,13 @@ var app = {
         //    }
         //});
       //  Open any external link with InAppBrowser Plugin
-        $(document).on('click', 'a[href^=http], a[href^=https]', function(e){
-            e.preventDefault();
-            var $this = $(this);
-            var target = $this.data('inAppBrowser') || '_blank';
-            //var ref = window.open('http://apache.org', '_blank', 'location=yes');
-            window.open($this.attr('href'), target);
-        });
+      //  $(document).on('click', 'a[href^=http], a[href^=https]', function(e){
+      //      e.preventDefault();
+      //      var $this = $(this);
+      //      var target = $this.data('inAppBrowser') || '_blank';
+      //      //var ref = window.open('http://apache.org', '_blank', 'location=yes');
+      //      window.open($this.attr('href'), target);
+      //  });
         document.addEventListener("deviceready", onDeviceReady, false);
         function onDeviceReady() {
             console.log("window.open works well");
@@ -128,15 +111,15 @@ var app = {
     app.controller('HomeController', function($scope, Data) {
 
 //Open any external link with InAppBrowser Plugin
-        $(document).on('click', 'a[href^=http], a[href^=https]', function(e){
-
-            e.preventDefault();
-            var $this = $(this);
-            var target = $this.data('inAppBrowser') || '_system';
-
-            window.open($this.attr('href'), target);
-
-        });
+//        $(document).on('click', 'a[href^=http], a[href^=https]', function(e){
+//
+//            e.preventDefault();
+//            var $this = $(this);
+//            var target = $this.data('inAppBrowser') || '_system';
+//
+//            window.open($this.attr('href'), target);
+//
+//        });
 
         $scope.items = Data.items;
 
@@ -271,15 +254,15 @@ var app = {
     app.controller('FoodMenuController', function($scope, $http, FoodMenuData) {
 
 // Open any external link with InAppBrowser Plugin
-        $(document).on('click', 'a[href^=http], a[href^=https]', function(e){
-
-            e.preventDefault();
-            var $this = $(this);
-            var target = $this.data('inAppBrowser') || '_blank';
-
-            window.open($this.attr('href'), target);
-
-        });
+//        $(document).on('click', 'a[href^=http], a[href^=https]', function(e){
+//
+//            e.preventDefault();
+//            var $this = $(this);
+//            var target = $this.data('inAppBrowser') || '_blank';
+//
+//            window.open($this.attr('href'), target);
+//
+//        });
 
         $scope.food = [];
 
